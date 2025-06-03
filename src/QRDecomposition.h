1e5d382019faf02fd4f3f9b55aee7a3575c3224a
Alexey Kandrin <adkandrin@gmail.com>
2025-06-03 16:34:17 +0300

    Revert "матрица хранится как лента"; блочное матричное умножение
    # Conflicts:
    #	src/QRDecomposition.h

--- src/QRDecomposition.h
+++ src/QRDecomposition.h
@@ -14,31 +14,31 @@ inline static float sign(float x)
 template <typename T>
 static float count_beta(const TMatrix<T>& Ak, int k_step)
 {
-	const size_t N = Ak.GetN();
+	const size_t N = Ak.Size();
     float sum_by_k_col = 0.0;
     for (size_t str = k_step; str < N; ++str)
     {
-		sum_by_k_col += Ak.get(str, k_step) * Ak.get(str, k_step);
+		sum_by_k_col += Ak[str][k_step] * Ak[str][k_step];
     }
-	auto beta = sign(-Ak.get(k_step, k_step)) * sqrt(sum_by_k_col);
+	auto beta = sign(-Ak[k_step][k_step]) * sqrt(sum_by_k_col);
     return beta;
 }
 
 template <typename T>
 static float count_mu(float beta, int k_step, const TMatrix<T>& Ak)
 {
-	auto mu = 1 / sqrt(2.0 * beta * beta - 2 * beta * Ak.get(k_step, k_step));
+	auto mu = 1 / sqrt(2.0 * beta * beta - 2 * beta * Ak[k_step][k_step]);
     return mu;
 }
 
 template <typename T>
 static TVector<T> count_w(const TMatrix<T>& Ak, int k_step, float beta, float mu)
 {
-	const size_t N = Ak.GetN();
+	const size_t N = Ak.Size();
     TVector<T> w(N);
     for (size_t str = 0; str < N; ++str)
     {
-		w[str] = str < k_step ? 0 : Ak.get(str, k_step) * mu;
+		w[str] = str < k_step ? 0 : Ak[str][k_step] * mu;
     }
 
     w[k_step] -= beta * mu;
@@ -52,7 +52,7 @@ static TMatrix<T> count_H(float beta, float mu, TVector<T> w)
     TMatrix<T> E(N, N);
     for (size_t i = 0; i < N; ++i)
     {
-		E.get(i,i) = 1.0;
+		E[i][i] = 1.0;
     }
     auto H = E - w * w * 2.0;
     return H;
@@ -76,7 +76,7 @@ static void count_Q(const TMatrix<T>& H, TMatrix<T>& Q) {
 template <typename T>
 void QR_decomposition(const TMatrix<T>& A, TMatrix<T>& Q, TMatrix<T>& R)
 {
-	const size_t N = A.GetN();
+	const size_t N = A.Size();
     R = A;
     for (size_t k = 0; k < N - 1; ++k)
     {
