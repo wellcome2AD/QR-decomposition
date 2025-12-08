279104d47756e11a649496096e961e7b7104635b
wellcome2AD <iuuyyttttt@mail.ru>
2025-12-08 19:48:14 +0300

    работающая версия; исправлена структура тестовой папки

--- src/GivensQRDecomposition.h
+++ src/GivensQRDecomposition.h
@@ -6,67 +6,40 @@
 #include "TMatrix.h"
 
 template <typename T>
-void Givens_QR_decomposition(const TMatrix<T> &A, TMatrix<T> &Q, TMatrix<T> &R)
+void Givens_QR_decomposition(const TMatrix<T>& A, TMatrix<T>& Q, TMatrix<T>& R)
 {
-    auto n = A.Size(), m = A[0].Size();
-    R = A;
-    bool isFirst = true;
-    for (auto j = 0; j < m - 1; j++)
-    { // зануляем весь столбец j
-        for (auto i = j + 1; i < n; i++)
-        { // c j+1 строк
-            std::cout << "i=" << i << ", j=" << j << std::endl;
+	auto n = A.Size(), m = A[0].Size();
+	R = A;
 
-            auto sqrt = std::sqrt(R[j][j] * R[j][j] + R[i][j] * R[i][j]);
-            double c = R[j][j] / sqrt, s = -R[i][j] / sqrt;
-            std::cout << "c=" << c << ", s=" << s << std::endl;
+	Q = TMatrix<T>(n, n, 0);
+	for (auto i = 0; i < n; i++)
+	{
+		Q[i][i] = 1;
+	}
 
-            for (auto k = j; k < m; k++)
-            { // вращаем только две строки -- i и j
-                auto temp = R[j][k] * c - R[i][k] * s;
-                R[i][k] = R[j][k] * s + R[i][k] * c;
-                R[j][k] = temp;
-            }
-            std::cout << "R:" << R;
-            std::cout << std::endl;
+	for (auto j = 0; j < m - 1; j++)
+	{ // зануляем весь столбец j
+//#pragma omp parallel for num_threads(thread_num)// if(n > 500)
+		for (auto i = j + 1; i < n; i++)
+		{ // c j+1 строк
+			auto sqrt = std::sqrt(R[j][j] * R[j][j] + R[i][j] * R[i][j]);
+			double c = R[j][j] / sqrt, s = -R[i][j] / sqrt;
+			//#pragma omp parallel for num_threads(thread_num)// if(n > 500)
+			for (auto k = j; k < m; k++)
+			{ // вращаем только две строки -- i и j
+				auto temp = R[j][k] * c - R[i][k] * s;
+				R[i][k] = R[j][k] * s + R[i][k] * c;
+				R[j][k] = temp;
+			}
 
-            if (isFirst)
-            {
-                Q = TMatrix<T>(n, n, 0);
-                for (auto i = 0; i < n; i++)
-                {
-                    Q[i][i] = 1;
-                }
-                Q[0][0] = Q[1][1] = c;
-                Q[0][1] = -s;
-                Q[1][0] = s;
-                isFirst = false;
-                // std::cout << "Q:" << Q;
-                // std::cout << std::endl;
-            }
-            else
-            {
-                for (auto k = j; k < n; k++)
-                { // меняются только две строки -- i и j
-                    auto temp = Q[j][k] * c - Q[i][k] * s;
-                    Q[i][k] = Q[j][k] * s + Q[i][k] * c;
-                    Q[j][k] = temp;
-                }
-            }
-            std::cout << "Q:" << Q;
-            std::cout << std::endl;
-            std::cout << "-------------------\n";
-        }
-    }
-
-    // транспонировать Q
-    for (auto i = 0; i < n - 1; i++)
-    {
-        for (auto j = i + 1; j < m; j++)
-        {
-            std::swap(Q[i][j], Q[j][i]);
-        }
-    }
-    // std::cout << "Q:" << Q;
-    // std::cout << std::endl;
+			//#pragma omp parallel for num_threads(thread_num)// if(n > 500)
+			for (auto k = 0; k < n; k++)
+			{ // меняются только две строки -- i и j
+				auto temp = c * Q[k][j] - s * Q[k][i];
+				Q[k][i] = s * Q[k][j] + c * Q[k][i];
+				Q[k][j] = temp;
+			}
+		}
+	}
 }
+
